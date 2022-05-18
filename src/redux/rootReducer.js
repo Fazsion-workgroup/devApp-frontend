90c4518c985e84588fa5bead4240ccb27a228960
Daetoun <97284852+Daetoun@users.noreply.github.com>
2022-05-18 03:52:45 +0100

    Initial Commit and folder setup

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,16 @@
+//setting up redux store
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["userReducer"],
+};
+
+const rootReducer = combineReducers({
+  //   marketReducer,
+});
+
+export default persistReducer(persistConfig, rootReducer);
